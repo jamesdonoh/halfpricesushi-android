03ed2016c81d22b65fe0fc8d31446966605d395c
James Donohue <james.brackish@gmail.com>
2017-01-22 17:08:20 +0000

    Add toolbar to details activity

--- app/src/main/java/io/github/jamesdonoh/halfpricesushi/OutletDetailsActivity.java
+++ app/src/main/java/io/github/jamesdonoh/halfpricesushi/OutletDetailsActivity.java
@@ -3,6 +3,7 @@ package io.github.jamesdonoh.halfpricesushi;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class OutletDetailsActivity extends AppCompatActivity {
     @Override
@@ -14,6 +15,10 @@ public class OutletDetailsActivity extends AppCompatActivity {
             return;
         }
 
+        setContentView(R.layout.activity_outlet_details);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
         if (savedInstanceState == null) {
             // Activity is being initialised for first time, so create details fragment
             OutletDetailsFragment detailsFragment = new OutletDetailsFragment();
@@ -21,9 +26,8 @@ public class OutletDetailsActivity extends AppCompatActivity {
             // Pass outlet ID from extra to fragment as argument
             detailsFragment.setArguments(getIntent().getExtras());
 
-            // Add fragment to the root view of the activity
-            int rootViewId = android.R.id.content;
-            getSupportFragmentManager().beginTransaction().add(rootViewId, detailsFragment).commit();
+            // Add fragment to content frame of activity layout
+            getSupportFragmentManager().beginTransaction().add(R.id.content, detailsFragment).commit();
         }
     }
 }

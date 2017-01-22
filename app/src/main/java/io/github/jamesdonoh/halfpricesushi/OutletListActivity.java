7d910faa94409fd02e367b731490979208eb7d2b
James Donohue <james.brackish@gmail.com>
2017-01-22 20:29:15 +0000

    Move sort order toggle handling to List Fragment from Activity

--- app/src/main/java/io/github/jamesdonoh/halfpricesushi/OutletListActivity.java
+++ app/src/main/java/io/github/jamesdonoh/halfpricesushi/OutletListActivity.java
@@ -16,25 +16,4 @@ public class OutletListActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_outlet_list, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.toggle_sort:
-                // User chose the toggle sort button
-                Toast.makeText(getApplicationContext(), R.string.sort_toggle, Toast.LENGTH_SHORT).show();
-                return true;
-
-            default:
-                // Action not recognized so pass to superclass
-                return super.onOptionsItemSelected(item);
-        }
-    }
 }
--- app/src/main/java/io/github/jamesdonoh/halfpricesushi/OutletListFragment.java
+++ app/src/main/java/io/github/jamesdonoh/halfpricesushi/OutletListFragment.java
@@ -7,12 +7,13 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import java.util.List;
-
-import io.github.jamesdonoh.halfpricesushi.model.Outlet;
 import io.github.jamesdonoh.halfpricesushi.model.OutletStore;
 
 public class OutletListFragment extends Fragment implements OutletAdapter.OnOutletClickListener {
@@ -30,6 +31,9 @@ public class OutletListFragment extends Fragment implements OutletAdapter.OnOutl
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("OutletListFragment", "onCreate(" + (savedInstanceState != null ? "Bundle" : "") + ")");
+
+        // This fragment would like to participate in populating the options menu
+        setHasOptionsMenu(true);
 
         if (savedInstanceState != null) {
             mSelectedOutletId = savedInstanceState.getInt(SELECTED_OUTLET_ID);
@@ -76,6 +80,26 @@ public class OutletListFragment extends Fragment implements OutletAdapter.OnOutl
         super.onSaveInstanceState(outState);
         Log.d("OutletListFragment", "onSaveInstanceState: persisting selected ID = " + mSelectedOutletId);
         outState.putInt(SELECTED_OUTLET_ID, mSelectedOutletId);
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_outlet_list, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.toggle_sort:
+                // User chose the toggle sort button
+                Toast.makeText(getContext(), R.string.sort_toggle, Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                // Action not recognized so pass to superclass
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override

4f6864174c2956f9901b6ef6ec1fadb02073d9ce
James Donohue <james.brackish@gmail.com>
2017-01-23 20:38:23 +0000

    Add google map to map view fragment

--- app/src/main/java/io/github/jamesdonoh/halfpricesushi/OutletMapFragment.java
+++ app/src/main/java/io/github/jamesdonoh/halfpricesushi/OutletMapFragment.java
@@ -6,10 +6,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class OutletMapFragment extends Fragment {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class OutletMapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_outlet_map, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }

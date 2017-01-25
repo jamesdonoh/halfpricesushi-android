308c5840d592bd64750859381f003a24977ac776
James Donohue <james.brackish@gmail.com>
2017-01-25 10:42:07 +0000

    Check for permission before enabling My Location button

--- app/src/main/java/io/github/jamesdonoh/halfpricesushi/OutletMapFragment.java
+++ app/src/main/java/io/github/jamesdonoh/halfpricesushi/OutletMapFragment.java
@@ -1,7 +1,10 @@
 package io.github.jamesdonoh.halfpricesushi;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,7 +21,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import io.github.jamesdonoh.halfpricesushi.model.Outlet;
 import io.github.jamesdonoh.halfpricesushi.model.OutletStore;
 
-public class OutletMapFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
+public class OutletMapFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener, GoogleMap.OnMyLocationButtonClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -45,6 +48,21 @@ public class OutletMapFragment extends Fragment implements OnMapReadyCallback, G
 
         googleMap.setOnInfoWindowClickListener(this);
 
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getContext(), "Location permission granted :)",
+                    Toast.LENGTH_SHORT).show();
+            googleMap.setMyLocationEnabled(true);
+            //googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+            googleMap.setOnMyLocationButtonClickListener(this);
+        } else {
+            // TODO request permission/handle gracefully
+            // https://developer.android.com/training/permissions/requesting.html
+            // https://github.com/googlemaps/android-samples/blob/master/ApiDemos/app/src/main/java/com/example/mapdemo/MyLocationDemoActivity.java
+            Toast.makeText(getContext(), "Location permission denied :(",
+                    Toast.LENGTH_LONG).show();
+        }
+
         LatLng oxfordCircus = new LatLng(51.515514, -0.141864);
         float zoom = 12;
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(oxfordCircus, zoom));
@@ -55,5 +73,14 @@ public class OutletMapFragment extends Fragment implements OnMapReadyCallback, G
         // TODO make this open details activity - but what to do in landscape mode?
         Toast.makeText(getContext(), "Info window clicked",
                 Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        // TODO this is just a a copy-paste - maybe remove if we don't need custom behaviour
+        Toast.makeText(getContext(), "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
     }
 }

6e0136658d43ded03dd21119338ccf2a00e76703
James Donohue <james.brackish@gmail.com>
2017-01-29 12:28:27 +0000

    Make JSONArrayRequest in splash activity; allow cancellation; handle Volley result in splash activity

--- app/src/main/java/io/github/jamesdonoh/halfpricesushi/SplashActivity.java
+++ app/src/main/java/io/github/jamesdonoh/halfpricesushi/SplashActivity.java
@@ -5,29 +5,48 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import io.github.jamesdonoh.halfpricesushi.model.OutletStore;
+import org.json.JSONArray;
+
+import io.github.jamesdonoh.halfpricesushi.model.OutletApi;
 
 /**
  * Branded launch screen, following advice at
  * https://material.io/guidelines/patterns/launch-screens.html#launch-screens-branded-launch
  */
 public class SplashActivity extends AppCompatActivity
-        implements DataErrorDialogFragment.DataErrorDialogListener {
+        implements DataErrorDialogFragment.DataErrorDialogListener, OutletApi.DataHandler {
+    private OutletApi mApi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        checkOutletDataAndContinue();
+        mApi = OutletApi.getInstance(this);
+        requestApiData();
     }
 
-    private void checkOutletDataAndContinue() {
-        if (OutletStore.getNumOutlets(this) == 0) {
-            // A problem occurred fetching data, prompt to retry
-            showDataErrorDialog();
-        } else {
-            // Otherwise continue to main finder activity
-            startFinderActivity();
+    @Override
+    protected void onStop () {
+        super.onStop();
+
+        if (mApi != null) {
+            mApi.cancelAllRequests();
         }
+    }
+
+    public void onDataLoaded(JSONArray jsonArray) {
+        // Got API data, cool
+        // TODO store the API data in the database
+        startFinderActivity();
+    }
+
+    public void onError(Exception error) {
+        // Allow user to retry request
+        showDataErrorDialog();
+    }
+
+    public void onRetryClick() {
+        requestApiData();
     }
 
     private void showDataErrorDialog() {
@@ -35,14 +54,14 @@ public class SplashActivity extends AppCompatActivity
         dialog.show(getSupportFragmentManager(), "DataErrorDialogFragment");
     }
 
-    public void startFinderActivity() {
+    private void requestApiData() {
+        mApi.loadData(this);
+    }
+
+    private void startFinderActivity() {
         // Start finder activity immediately
         Intent intent = new Intent(this, OutletFinderActivity.class);
         startActivity(intent);
         finish();
-    }
-
-    public void onRetryClick() {
-        checkOutletDataAndContinue();
     }
 }
--- app/src/main/java/io/github/jamesdonoh/halfpricesushi/model/OutletApi.java
+++ app/src/main/java/io/github/jamesdonoh/halfpricesushi/model/OutletApi.java
@@ -7,8 +7,12 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+
+import io.github.jamesdonoh.halfpricesushi.R;
 
 /**
  * Represents the outlet offer database API and provides convenience methods to query and
@@ -17,7 +21,7 @@ import com.android.volley.toolbox.Volley;
  * Follows singleton pattern recommended by
  * https://developer.android.com/training/volley/requestqueue.html
  */
-class OutletApi {
+public class OutletApi {
     private static final String TAG = OutletApi.class.getSimpleName();
 
     // TODO deal with warning (see below)
@@ -27,9 +31,14 @@ class OutletApi {
 
     private RequestQueue mRequestQueue;
 
+    private String mBaseUrl;
+
     private OutletApi(Context context) {
         mContext = context;
         mRequestQueue = getRequestQueue();
+
+        // TODO make this not depend on R (non-model concern)
+        mBaseUrl = context.getString(R.string.offer_api_url);
     }
 
     public static synchronized OutletApi getInstance(Context context) {
@@ -40,6 +49,43 @@ class OutletApi {
         return sInstance;
     }
 
+    public void cancelAllRequests() {
+        mRequestQueue.cancelAll(TAG);
+    }
+
+    public void loadData(final DataHandler handler) {
+        RequestQueue queue = Volley.newRequestQueue(mContext);
+        String url = mBaseUrl + "/outlets/all";
+
+        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.i(TAG, "onResponse: " + response.toString().substring(0, 100));
+                        handler.onDataLoaded(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e(TAG, "onResponse - error", error);
+                        handler.onError(error);
+                    }
+                }
+        );
+
+        // Set the tag on the request so it can be cancelled later
+        jsonObjectRequest.setTag(TAG);
+
+        Log.i(TAG, "loadData: queueing request for " + url);
+        queue.add(jsonObjectRequest);
+    }
+
+    public interface DataHandler {
+        void onDataLoaded(JSONArray data);
+        void onError(Exception exception);
+    }
+
     private RequestQueue getRequestQueue() {
         if (mRequestQueue == null) {
             // Must use application context to ensure this lasts lifetime of app
@@ -47,29 +93,5 @@ class OutletApi {
         }
 
         return mRequestQueue;
-    }
-
-    void fetchOutlets(Context context, String baseUrl) {
-        RequestQueue queue = Volley.newRequestQueue(context);
-        //String baseUrl = getString(R.string.offer_api_url);
-        String url = baseUrl + "/outlets/all";
-
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Log.i(TAG, "onResponse: " + response.substring(0, 100));
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e(TAG, "onResponse - error", error);
-                    }
-                }
-        );
-
-        Log.i(TAG, "loadData: queueing request for " + url);
-        queue.add(stringRequest);
     }
 }

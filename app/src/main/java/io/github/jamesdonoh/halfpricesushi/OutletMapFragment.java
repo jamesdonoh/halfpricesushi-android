9aeef915b2c48fec39a47e01082acb51a93ab38c
James Donohue <james.brackish@gmail.com>
2017-01-29 13:26:31 +0000

    Use sushi icon as map marker

--- app/src/main/java/io/github/jamesdonoh/halfpricesushi/OutletMapFragment.java
+++ app/src/main/java/io/github/jamesdonoh/halfpricesushi/OutletMapFragment.java
@@ -12,6 +12,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -41,12 +43,15 @@ public class OutletMapFragment extends Fragment implements OnMapReadyCallback, G
     public void onMapReady(GoogleMap map) {
         mMap = map;
 
+        BitmapDescriptor bitmapDescriptor = BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher);
+
         for (Outlet outlet : OutletStore.getAllOutlets(getContext())) {
             // sushi = "\ud83c\udf63";
             LatLng position = new LatLng(outlet.getLatitude(), outlet.getLongitude());
             mMap.addMarker(new MarkerOptions()
                     .position(position)
-                    .title(outlet.getName()));
+                    .title(outlet.getName())
+                    .icon(bitmapDescriptor));
         }
 
         mMap.setOnInfoWindowClickListener(this);

37e910f18cef66980f46be3e50538b92f8eb24d0
James Donohue <james.brackish@gmail.com>
2017-01-30 20:05:19 +0000

    OutletTest unit test

---
+++ app/src/test/java/io/github/jamesdonoh/halfpricesushi/model/OutletTest.java
@@ -0,0 +1,17 @@
+package io.github.jamesdonoh.halfpricesushi.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Sample local unit test for Outlet class.
+ *
+ * This will execute on the development machine.
+ */
+public class OutletTest {
+    @Test
+    public void outlet_something() {
+        assertTrue(true);
+    }
+}

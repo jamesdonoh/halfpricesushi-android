f44cbca0c96e1034a6843e5c2aca44691bd4f713
James Donohue <james.brackish@gmail.com>
2017-01-30 20:14:54 +0000

    Use @Before for setup

--- app/src/test/java/io/github/jamesdonoh/halfpricesushi/model/OutletTest.java
+++ app/src/test/java/io/github/jamesdonoh/halfpricesushi/model/OutletTest.java
@@ -1,17 +1,31 @@
 package io.github.jamesdonoh.halfpricesushi.model;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 /**
- * Sample local unit test for Outlet class.
+ * Example local unit test for Outlet class.
  *
  * This will execute on the development machine.
  */
 public class OutletTest {
+    Outlet outlet;
+
+    @Before
+    public void initialise() {
+        outlet = new Outlet(1, "Oxford Circus", 51.515419, -0.141099);
+    }
+
     @Test
-    public void outlet_something() {
-        assertTrue(true);
+    public void outlet_isRated_WithoutRating() {
+        assertFalse(outlet.isRated());
+    }
+
+    @Test
+    public void outlet_isRated_WithRating() {
+        outlet.setRating(1);
+        assertTrue(outlet.isRated());
     }
 }
